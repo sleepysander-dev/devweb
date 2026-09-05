0c82ac3eb17ca87f32655059e374af08dd011fdc
sleepysander-dev <racoondumpster01@gmail.com>
2026-09-05 18:18:07 +0200

    Omschrijving van wat je hebt aangepast

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cdAudio = document.getElementById('cd-audio');
 const cdKnop = document.getElementById('cd-knop');
 const cdSchijf = document.getElementById('cd-schijf');
+const cdVolume = document.getElementById('cd-volume');
+
+cdAudio.volume = cdVolume.value;
 
 cdKnop.addEventListener('click', () => {
     if (cdAudio.paused) {
@@ -12,4 +15,8 @@ cdKnop.addEventListener('click', () => {
         cdSchijf.classList.remove('actief');
         cdKnop.innerHTML = '<i class="fa-solid fa-play"></i>';
     }
+});
+
+cdVolume.addEventListener('input', () => {
+    cdAudio.volume = cdVolume.value;
 });
